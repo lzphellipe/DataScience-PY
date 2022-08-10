50f8cf70db76aa9696d0eb12556c796b8faf26e7
Luiz Felipe Alves Lopes <luizfelipealveslopes9@gmail.com>
2022-08-10 11:39:45 -0300

    conteudo curso PY-R
     COMMIT INIT

---
+++ fundamentos.py
@@ -0,0 +1,66 @@
+# criar variavel do tipo inteiro
+from ast import Str
+from traceback import print_tb
+
+
+x = 1
+
+# criar variavel do tipo float
+
+y = 3.13
+
+ # criar variavel do tipo string
+z = " fundamento Python"
+z = 'Fundamento Python'
+
+ # Criar variavel do tipo lógica ( true ou false)
+ w = true
+ y = false
+
+   #principais operadores
+
+   +, -, /, *
+
+   x = 10;
+   y = 20;
+   z = 100;
+   calculo = (x+y) *z/100
+    print(calculo);
+
+    #exibir valores no console
+
+    print(" ESTE TEXTO SERÁ IMPRESSO NO CONSOLE")
+    PRINT(X);
+    print_tb(" TEXTO E DUAS VARIAVEIS", X,Z)
+
+     #verificar Tipo
+
+      type(x)
+
+      >>> x = 10
+      >>> type(x)
+      <class 'int'>
+
+      >>> y = "Python"
+      >>> type(y)
+      <class 'str'>
+
+      #ENTRADA DE VALORES 
+
+      x = input("infome um valor")
+
+      #CONVERSÃO DE VALORES
+
+      X= INT(z);
+      W = Str(m);
+      t= float(1)
+
+      ## comentário
+
+      ## texto ignorado
+      ## usado para lembretes e documentação do código
+
+      # comentario até o final da linha (#)
+    ``` comentario com varias linhas```
+
+    
